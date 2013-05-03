8c0b59f688bd9bf3c50e1e9b8b4a17183cb00395
Phil Battos <philbattos@gmail.com>
2013-05-02 21:32:29 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SoccerStore::Application.config.secret_token = '70d28d65d662625ca4fba70ba34534740d548e076ea8245d1b649e6569f07c3cfc05e9b9b9444998970bfb62ff38a0672c3eb55b12244917e151fd985897cf5f'
